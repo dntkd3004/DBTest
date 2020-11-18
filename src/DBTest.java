216e6463d56b18c9b64507432884daf909b66b12
dntkd3004 <dldntkd12@gmail.com>
2020-11-18 12:37:46 +0900

    데이터베이스

--- src/DBTest.java
+++ src/DBTest.java
@@ -7,8 +7,8 @@ import java.sql.SQLException;
 public class DBTest {
 
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
-		// DB 세팅
-		//DB 접속 정보
+		
+		//=======================================DB 세팅=========================================
 		
 		//드라이버 정보
 		String driver = "com.mysql.cj.jdbc.Driver";
@@ -21,10 +21,11 @@ public class DBTest {
 		
 		Class.forName(driver);	//Driver 세팅
 		Connection conn = DriverManager.getConnection(url, user, pass); //DBMS 선택 -> 담당자(Connection) 부여받음
-		// 조회
+		
+		//=========================================조회===========================================
 		String sql = "SELECT * FROM article";
 		
-		PreparedStatement pstmt = conn.prepareStatement(sql);	//prepareStatement 통해서 SQL 세팅
+		PreparedStatement pstmt = conn.prepareStatement(sql);	//PrepareStatement 통해서 SQL 세팅
 		
 		ResultSet rs = pstmt.executeQuery();	//조회 결과가 있는 경우
 		//pstmt.executeUpdate(); //조회 결과가 없는 경우
@@ -37,12 +38,15 @@ public class DBTest {
 			int hit = rs.getInt("hit");
 			System.out.println(id+""+title+""+body+""+nickname+""+hit+"");
 		}
-		// 삽입
-		String sql2 = "INSERT INTO article SET title = ?, `body` = ?, nickname = '홍길동', hit = 10";
-		pstmt.setString(1, "dfdf");
-		pstmt.setString(2, "kkkk");
 		
-		pstmt.executeUpdate(sql2);
+		//=========================================삽입===========================================
+		String sql2 = "INSERT INTO article SET title = ?, `body` = ?, nickname = '홍길동', hit = 10";
+		PreparedStatement pstmt2 = conn.prepareStatement(sql2);	//prepareStatement 통해서 SQL 세팅
+		
+		pstmt2.setString(1, "dfdf");
+		pstmt2.setString(2, "kkkk");
+		
+		pstmt2.executeUpdate(sql2);
 	}
 
 }
